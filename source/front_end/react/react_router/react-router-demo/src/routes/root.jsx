0dcf3e61e2ae1d3533c52a12e3733953064ce003
luguosong <1054595718@qq.com>
2024-05-17 08:55:54 +0800

    笔记 更新

--- source/front_end/react/react_router/react-router-demo/src/routes/root.jsx
+++ source/front_end/react/react_router/react-router-demo/src/routes/root.jsx
@@ -1,13 +1,20 @@
-import {Link, Outlet,useLoaderData} from "react-router-dom";
-import {getContacts} from "../contacts.js";
+import {Form, Link, NavLink, Outlet, redirect, useLoaderData, useNavigation} from "react-router-dom";
+import {createContact, getContacts} from "../contacts.js";
 
 export async function loader() {
     const contacts = await getContacts();
-    return { contacts };
+    return {contacts};
+}
+
+export async function action() {
+    const contact = await createContact();
+    // return { contact };
+    return redirect(`/contacts/${contact.id}/edit`);
 }
 
 export default function Root() {
-    const { contacts } = useLoaderData();
+    const {contacts} = useLoaderData();
+    const navigation = useNavigation();
     return (
         <>
             <div id="sidebar">
@@ -31,16 +38,25 @@ export default function Root() {
                             aria-live="polite"
                         ></div>
                     </form>
-                    <form method="post">
+                    <Form method="post">
                         <button type="submit">New</button>
-                    </form>
+                    </Form>
                 </div>
                 <nav>
                     {contacts.length ? (
                         <ul>
                             {contacts.map((contact) => (
                                 <li key={contact.id}>
-                                    <Link to={`contacts/${contact.id}`}>
+                                    <NavLink
+                                        to={`contacts/${contact.id}`}
+                                        className={({isActive, isPending}) =>
+                                            isActive
+                                                ? "active"
+                                                : isPending
+                                                    ? "pending"
+                                                    : ""
+                                        }
+                                    >
                                         {contact.first || contact.last ? (
                                             <>
                                                 {contact.first} {contact.last}
@@ -49,7 +65,7 @@ export default function Root() {
                                             <i>No Name</i>
                                         )}{" "}
                                         {contact.favorite && <span>★</span>}
-                                    </Link>
+                                    </NavLink>
                                 </li>
                             ))}
                         </ul>
@@ -60,8 +76,11 @@ export default function Root() {
                     )}
                 </nav>
             </div>
-            <div id="detail">
-                <Outlet />
+            <div id="detail"
+                 className={
+                     navigation.state === "loading" ? "loading" : ""
+                 }>
+                <Outlet/>
             </div>
         </>
     );
