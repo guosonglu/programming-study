b5ec7032555a0f4b9e99695dd8ce25a398b55af0
luguosong <1054595718@qq.com>
2024-09-24 09:03:09 +0800

    🎉 feat: 添加 Spring Boot Actuator 依赖并更新用户详情服务实现

--- code/java_serve/authentication/spring-security/02_hello/ssia-ch2-ex2/src/main/java/com/luguosong/ssiach2ex2/config/ProjectConfig.java
+++ code/java_serve/authentication/spring-security/02_hello/ssia-ch2-ex2/src/main/java/com/luguosong/ssiach2ex2/config/ProjectConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -32,9 +33,10 @@ public class ProjectConfig {
     }
 
 
+
     @Bean
     UserDetailsService userDetailsService() {
-        var user = User.withUsername("john")
+        UserDetails user = User.withUsername("john")
                 .password("12345")
                 .authorities("read")
                 .build();
