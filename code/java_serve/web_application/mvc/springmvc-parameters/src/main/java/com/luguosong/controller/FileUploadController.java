8a2a486c9d306c57c7221c08f7f7abdde3910ca9
luguosong <1054595718@qq.com>
2024-10-30 13:53:40 +0800

    🎉 feat: 添加用户名参数到文件上传功能

--- code/java_serve/web_application/mvc/springmvc-parameters/src/main/java/com/luguosong/controller/FileUploadController.java
+++ code/java_serve/web_application/mvc/springmvc-parameters/src/main/java/com/luguosong/controller/FileUploadController.java
@@ -12,44 +12,47 @@ import java.util.UUID;
 
 /**
  * 文件上传
+ *
  * @author luguosong
  */
 @Controller
 @RequestMapping("/fileUpload")
 public class FileUploadController {
 
-    @RequestMapping("/springMvc")
-    public String springMvc(
-            @RequestParam("fileName") MultipartFile file,
-            HttpServletRequest request) throws IOException {
-        System.out.println("请求参数名：" + file.getName());
-        String originalFilename = file.getOriginalFilename();
-        System.out.println("文件真实名称：" + originalFilename);
+	@RequestMapping("/springMvc")
+	public String springMvc(
+			@RequestParam("fileName") MultipartFile file,
+			@RequestParam("username") String username,
+			HttpServletRequest request) throws IOException {
+		System.out.println("请求参数名：" + file.getName());
+		String originalFilename = file.getOriginalFilename();
+		System.out.println("文件真实名称：" + originalFilename);
 
-        //将文件存储到服务端
-        //输入流
-        InputStream in = file.getInputStream();
-        BufferedInputStream bis = new BufferedInputStream(in);
-        //输出流
-        ServletContext application = request.getServletContext();//获取ServletContext
-        String folderPath = application.getRealPath("/upload");
-        File folder = new File(folderPath);
-        if (!folder.exists()) {
-            folder.mkdirs();
-        }
-        File saveFile = new File(folder.getAbsolutePath() + "/" + UUID.randomUUID().toString() + originalFilename.substring(originalFilename.lastIndexOf('.')));
-        BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(saveFile));
-        //开始传输文件
-        byte[] bytes = new byte[1024 * 10];
-        int readCount = 0;
-        while ((readCount = bis.read(bytes)) != -1) {
-            bos.write(bytes, 0, readCount);
-        }
-        bos.flush();
-        //释放资源
-        bos.close();
-        bis.close();
+		System.out.println("额外参数：" + username);
+		//将文件存储到服务端
+		//输入流
+		InputStream in = file.getInputStream();
+		BufferedInputStream bis = new BufferedInputStream(in);
+		//输出流
+		ServletContext application = request.getServletContext();//获取ServletContext
+		String folderPath = application.getRealPath("/upload");
+		File folder = new File(folderPath);
+		if (!folder.exists()) {
+			folder.mkdirs();
+		}
+		File saveFile = new File(folder.getAbsolutePath() + "/" + UUID.randomUUID().toString() + originalFilename.substring(originalFilename.lastIndexOf('.')));
+		BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(saveFile));
+		//开始传输文件
+		byte[] bytes = new byte[1024 * 10];
+		int readCount = 0;
+		while ((readCount = bis.read(bytes)) != -1) {
+			bos.write(bytes, 0, readCount);
+		}
+		bos.flush();
+		//释放资源
+		bos.close();
+		bis.close();
 
-        return "get-parameters/form";
-    }
+		return "get-parameters/form";
+	}
 }
